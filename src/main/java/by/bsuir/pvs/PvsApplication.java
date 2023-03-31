cb5e640e469af244cd60425a5043a51195572294
Alexey Burak <byraklesha@gmail.com>
2023-03-31 13:09:29 +0300

    init: build project

---
+++ src/main/java/by/bsuir/pvs/PvsApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.pvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsuir/pvs/PvsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.pvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
